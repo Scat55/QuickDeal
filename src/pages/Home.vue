c84e3c96fde223a7deadce91f408f94a33a82166
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-03-15 14:37:39 +0300

    feat: create input component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Input } from '@/shared/input';
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <Input type="text" placeholder="id пользователя" />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/shared/input/ui.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface Props {
+  type: string;
+  value?: string;
+  name?: string;
+  placeholder?: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <input class="input" :type="type" :value="value" :name="name" :placeholder="placeholder" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.input {
+  padding: 10px;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  color: #555;
+  outline: none;
+
+  &:focus {
+    border-color: #007bff;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  }
+}
+</style>
