2cf7733f334aa0d034660eb5bf6268005c0010c1
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-03-15 14:46:35 +0300

    feat: create container components

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { Input } from '@/shared/input';
+import { Container } from '@/shared/container';
 </script>
 
 <template>
-  <div>
-    <Input type="text" placeholder="id пользователя" />
-  </div>
+  <header class="header">
+    <Container>
+      <Input type="text" placeholder="id пользователя" />
+    </Container>
+  </header>
 </template>
 
 <style scoped></style>
---
+++ src/shared/container/ui.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.container {
+  max-width: 75.5rem;
+  margin: 0 auto;
+  padding: 0 0.9375rem;
+}
+</style>
