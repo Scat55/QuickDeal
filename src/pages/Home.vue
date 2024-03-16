8e57847fc96f60c40e00823d7dd63c75064a797c
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-03-16 16:55:16 +0300

    feat: create button component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,9 +3,12 @@ import { Input } from '@/shared/input';
 import { ref } from 'vue';
 import { usePersonStore } from '@/entities/person';
 import { Sidebar } from '@/widgets/sidebar';
+import { Button } from '@/shared/button';
+
 const personStore = usePersonStore();
 const id = ref<number | undefined>(undefined);
 const balanse = ref<number | undefined>(undefined);
+
 const getInfoAboutPerson = () => {
   if (id.value !== undefined) {
     personStore.getInfoAboutPerson(id.value);
@@ -17,13 +20,11 @@ const getInfoAboutPerson = () => {
 <template>
   <main class="main">
     <Sidebar class="main__sidebar">
-      <Input
-        type="number"
-        placeholder="id поьзователя"
-        v-model:value="id"
-        @keyup.enter="getInfoAboutPerson"
-      />
+      <Input type="number" placeholder="id поьзователя" v-model:value="id" />
       <Input type="number" placeholder="Баланс" v-model:value="balanse" />
+      <Button type="button" class="main__btn" @click="getInfoAboutPerson"
+        >Показать</Button
+      >
     </Sidebar>
   </main>
 </template>
@@ -37,6 +38,13 @@ const getInfoAboutPerson = () => {
     flex-direction: column;
     align-items: center;
     gap: 1.25rem;
+  }
+
+  &__btn {
+    &:hover {
+      color: white;
+      box-shadow: inset 0 -100px 0 0 #725ac1;
+    }
   }
 }
 </style>
---
+++ src/shared/button/ui.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  type: 'button' | 'submit' | 'reset' | undefined;
+}
+
+const { type } = defineProps<Props>();
+</script>
+
+<template>
+  <button class="button" :type="type"><slot></slot></button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  width: 100%;
+  height: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.625rem;
+  padding: 0 0.9375rem;
+  border-radius: 0.625rem;
+  color: #000;
+  border: 2px solid #ccc;
+  outline: none;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: ease-out 0.5s;
+}
+</style>
